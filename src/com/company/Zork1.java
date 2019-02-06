e5e318eb4ea572bba1bd60965ad2a3a15f64245f
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-05 19:32:17 -0500

    Updated

--- src/com/company/Zork1.java
+++ src/com/company/Zork1.java
@@ -2,22 +2,34 @@ package com.company;
 
 import java.util.Scanner;
 
+/**
+ *___________________________________
+ |          |           |     8     |
+ |     5    |    6      |___________|
+ |          |           |           |
+ |_____ ____|___________|_____7 ____|
+ |          |           |           |
+ |    3     |    2      |     4     |
+ |          |           |           |
+ |__________|___________|___________|
+ *          |           |
+ *          |      1    |
+ *          |           |
+ *          |___________|
+ */
 public class Zork1 {
-    static Scanner keyboard = new Scanner(System.in);
-    static String choice;
 
     public static void main(String[] args) {
-        System.out.println("Enter room \nChoice(n1)");
-
-        String choice;// = keyboard.next();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter house - Choice:(n1)");
+        String choice;
         char direction = '1';
         int room = 1;
-        while (true) { //room
-            System.out.print("\nEnter direction: ");
-            direction = keyboard.next().charAt(0);
-            System.out.print("Enter room: ");
-            room = keyboard.nextInt();
-            // room = Integer.parseInt(choice.substring(1,2));
+        while (true) {
+            System.out.print("Enter Choice: ");
+            choice = keyboard.next();
+            direction = choice.charAt(0);
+            room = Integer.parseInt(choice.substring(1,2));
             switch (room) { //Room
                 case 1:
                     room = foyer(direction);
@@ -46,46 +58,47 @@ public class Zork1 {
                 default:
                     System.out.println("Invalid Room");
                     System.exit(1);
+                    break;
             }
         }
     }
 
     public static int foyer(char direction) {
-        System.out.println("You are in foyer \n It contains : dead scorpion\n Choice (n2/x)");
+        System.out.println("Room: foyer \tcontains : dead scorpion\t Choices:(n2/s)");
         int room = 1;
         switch (direction) {
             case 'n':
                 room = 2;
                 break;
-            case 'x':
+            case 's':
                 System.exit(0);
             default:
-                System.out.println("Invalid direction");
+                System.out.println("1Invalid direction");
         }
         return room;
     }
 
     public static int frontRoom(char direction) {
-        System.out.println("You are in front Room \nIt contains : piano \nChoices (s1/w3/e4)");
+        System.out.println("Room: front Room \tContains : piano \tChoices:(s1/w3/e4)");
         int room = 2;
         switch (direction) {
             case 's':
                 room = 1;
                 break;
-            case 'e':
-                room = 4;
-                break;
             case 'w':
                 room = 3;
                 break;
+            case 'e':
+                room = 4;
+                break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("2Invalid direction");
         }
         return room;
     }
 
     public static int library(char direction) {
-        System.out.println("You are in the library \nIt contains : spider \nChoices (e2/n5)");
+        System.out.println("Room: library \tIt contains : spider \tChoices:(e2/n5)");
         int room = 3;
         switch (direction) {
             case 'e':
@@ -95,13 +108,13 @@ public class Zork1 {
                 room = 5;
                 break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("3Invalid direction");
         }
         return room;
     }
 
     public static int kitchen(char direction) {
-        System.out.println("You are in the Kitchen \nIt contains : bats \nChoices (w2/n7)");
+        System.out.println("Room: Kitchen \tIt contains : bats \tChoices:(w2/n7)");
         int room = 4;
         switch (direction) {
             case 'w':
@@ -115,20 +128,20 @@ public class Zork1 {
     }
 
     public static int diningRoom(char direction) {
-        System.out.println("You are in the Dining Room \nIt contains : dust empty box \nChoice (r3)");
+        System.out.println("Room: Dining Room \tIt contains : dust empty box \tChoice:(r3)");
         int room = 5;
         switch (direction) {
             case 's':
                 room = 3;
                 break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("5Invalid direction");
         }
         return room;
     }
 
     public static int vault(char direction) {
-        System.out.println("You are in the vault\nIt contains : 3 walking skeletons \nChoices (e7/e8)");
+        System.out.println("Room: vault\tIt contains : 3 walking skeletons \tChoices:(e7/e8)");
         int room = 6;
         switch (direction) {
             case 'e':
@@ -140,36 +153,36 @@ public class Zork1 {
                 }
                 break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("6Invalid direction");
         }
         return room;
     }
 
     public static int parlor(char direction) {
-        System.out.println("You are in the parlor\nIt contains : treasure chest\nChoices(w6/s4)");
+        System.out.println("Room: parlor\tContains : treasure chest\tChoices:(w6/s4)");
         int room = 7;
         switch (direction) {
-            case 's':
-                room = 4;
-                break;
             case 'w':
                 room = 6;
                 break;
+            case 's':
+                room = 4;
+                break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("7Invalid direction");
         }
         return room;
     }
 
     public static int secretRoom(char direction) {
-        System.out.println("You are in the secret room\nIt contains : piles of gold\nChoices (w6)");
+        System.out.println("Room: Secret \tContains : piles of gold\tChoices (w6)");
         int room = 8;
         switch (direction) {
             case 'w':
                 room = 6;
                 break;
             default:
-                System.out.println("Invalid direction");
+                System.out.println("8Invalid direction");
         }
         return room;
     }
